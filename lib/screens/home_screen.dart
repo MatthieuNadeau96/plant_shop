98eb4d2214a4976e4d0993ea9c3d58c7f6fdc43a
Matthieu <matthieunadeau96@gmail.com>
2020-12-06 12:14:42 -0500

    Added searchbar and title to the HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:plant_shop/main.dart';
 import 'package:plant_shop/widgets/my_app_bar.dart';
 
@@ -10,9 +11,79 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    Size deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: MyAppBar(
         title: 'Home',
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(
+              bottom: 15,
+              left: 30,
+              top: 30,
+            ),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Plant Catalog',
+                style: TextStyle(
+                  color: theme.textTheme.bodyText1.color,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                    height: 50,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(
+                          Icons.search,
+                          color: Colors.grey[400],
+                          size: 30,
+                        ),
+                        hintText: 'Search',
+                        hintStyle: TextStyle(
+                          color: Colors.grey[400],
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 15),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.green,
+                  ),
+                  child: Icon(
+                    Icons.tune,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
